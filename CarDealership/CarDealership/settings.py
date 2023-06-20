35fe0b37c1418b60b7dbcf2486b886e6fdf0572b
maya <vvvoyshnism@gmail.com>
2023-06-20 16:23:03 +0300

    add: models

--- CarDealership/CarDealership/settings.py
+++ CarDealership/CarDealership/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_countries',
+    'CarDealership_app',
 ]
 
 MIDDLEWARE = [
@@ -75,10 +77,15 @@ WSGI_APPLICATION = 'CarDealership.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'cardealership',
+        'USER': 'cduser',
+        'PASSWORD': '4236',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
